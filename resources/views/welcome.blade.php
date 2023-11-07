c39f9b5887509f6ddf689ee6722d4402cdbc08fd
pratishma09 <velv9tkiss3s@gmail.com>
2023-11-07 18:11:24 +0545

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Project</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <h1 class="heading">Blogs Today</h1>
+    <article>
+    <h1><a href="/post">Mindfulness in Daily Life</a></h1>
+    <p>In our fast-paced and increasingly hectic world, the importance of mindfulness in daily life cannot be overstated. Mindfulness is a practice that encourages us to be fully present in the moment, to pay attention to our thoughts, feelings, and the world around us without judgment. It offers a simple yet powerful way to reduce stress, enhance focus, and improve our overall well-being. By taking just a few minutes each day to engage in mindfulness exercises, such as deep breathing or meditation, we can cultivate a greater sense of self-awareness and emotional resilience. This not only helps us better manage the challenges that life throws at us but also allows us to savor the small joys and moments of beauty that often go unnoticed in our busy lives.</p>
+    </article>
+
+    <article>
+    <h1><a href="/post">Benefits of Regular Exercise</a></h1>
+    <p>Regular exercise is not just a means to stay physically fit; it's a holistic approach to a healthier and happier life. The benefits of incorporating exercise into your daily routine extend far beyond a slimmer waistline. Physical activity has been proven to boost our mood, reduce stress and anxiety, and increase our overall energy levels. It's a powerful way to improve cardiovascular health, increase muscle strength, and enhance flexibility. Moreover, regular exercise can also improve cognitive function, aiding in better decision-making, sharper memory, and increased creativity. </p>
+    </article>
+
+    <article>
+    <h1><a href="/post">The Power of Gratitude in Daily Living</a></h1>
+    <p>Gratitude is a simple yet transformative practice that can bring more joy and fulfillment into our daily lives. It's easy to get caught up in the hustle and bustle of our routines, often taking the blessings we have for granted. However, by taking a moment each day to reflect on and appreciate the people, experiences, and things we have, we can shift our perspective and find contentment in the present moment. Gratitude has been linked to improved mental health, reduced stress, and stronger relationships. It helps us focus on the positive aspects of our lives, even during challenging times, and can lead to a greater sense of inner peace. </p>
+    </article>
+</body>
+</html>
